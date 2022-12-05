1b670ea91ebbf8a9a7b12003fbbd0586e6d96da9
ktonk <ktonk@DESKTOP-TVRDP29>
2022-12-05 15:25:37 +0300

    Добавьте файлы проекта.

---
+++ AppPresentation/LoginWindow.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System.Windows;
+using System.Windows.Input;
+
+namespace View
+{
+    /// <summary>
+    /// Логика взаимодействия для LoginWindow.xaml
+    /// </summary>
+    public partial class LoginWindow : Window
+    {
+        public LoginWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void mainBorderMouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.ChangedButton == MouseButton.Left) this.DragMove();
+        }
+        private void minimizeButtonClick(object sender, RoutedEventArgs e)
+        {
+            this.WindowState = WindowState.Minimized;
+        }
+        private void closeButtonClick(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
---
+++ AppPresentation/MainWindow.xaml.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace View
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private bool IsMaximized = false;
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        private void mainBorderMouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.ChangedButton == MouseButton.Left) this.DragMove();
+        }
+        private void maximizeButtonClick(object sender, RoutedEventArgs e)
+        {
+            switch (IsMaximized)
+            {
+                case true:
+                    this.WindowState = WindowState.Normal;
+                    this.Width = 1080;
+                    this.Height = 720;
+                    IsMaximized = false;
+                    break;
+
+                case false:
+                    this.WindowState = WindowState.Maximized;
+                    IsMaximized = true;
+                    break;
+            }
+        }
+        private void exitButtonClick(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
+
